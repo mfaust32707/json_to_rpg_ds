2e26984145f5277a369082bb1cc1826a46c4eb84
mfaust32707 <mike@faustarts.com>
2022-11-23 19:23:36 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -24,25 +24,25 @@ function getStruct(object, level) {
       if ((typeof object[key][0]) !== "object") {
         var typeNew = typeof object[key][0];
         if (typeNew === "string")
-          type = "varchar(" + (object[key][0].length * 2).toString() + ")  dim(99)\n"
+          type = "varchar(" + (object[key][0].length * 2).toString() + ")  dim(99);\n";
       }
       if ((typeof object[key][0]) == 'object') {
-        rtnVal += spaces.substring(0,level) + "dcl-ds " + key + " dim(99);\n"
+        rtnVal += spaces.substring(0,level) + "dcl-ds " + key + " dim(99);\n";
         getStruct(object[key][0], level + 1);
-        rtnVal += spaces.substring(0,level) + "end-ds;\n"
+        rtnVal += spaces.substring(0,level) + "end-ds;\n";
     }
       
     } else if (type === "string") {
-      type = "varchar(" + (object[key].length * 2).toString() + ")"
+      type = "varchar(" + (object[key].length * 2).toString() + ")";
     }
 
     if (type == 'object') {
       rtnVal += spaces.substring(0,level) + "dcl-ds " + key + ";\n" +
         getStruct(object[key], level + 1);
-      rtnVal += spaces.substring(0,level) + "end-ds;\n"
+      rtnVal += spaces.substring(0,level) + "end-ds;\n";
     } else {
       rtnVal += spaces.substring(0,level) + key + " " + type + ";\n";
     }
-  })
+  });
   return rtnVal;
 }
